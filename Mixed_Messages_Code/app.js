37e5185b7e0d8745f699054c15e2e8d3971064e5
MBorko793 <79283620+MBorko793@users.noreply.github.com>
2021-08-14 14:49:41 -0400

    Update app.js

--- Mixed_Messages_Code/app.js
+++ Mixed_Messages_Code/app.js
@@ -1,4 +1,10 @@
 'use strict';
 
-console.log('hello world! YEET!');
+let msgtime = ['In the winter', 'In the summer', 'When the moon reaches its apex', 'At twelve oclock on a sunday', 'On the Fourth of July', 'On your next birthday'];
+let msgplace = ['at the mall', 'in a car', 'in the pool', 'in Las Vegas', 'under the apple tree', 'back behind the shed'];
+let msgaction = ['you will meet the love of your life.', 'you will fall into a terrible situation.', 'you will meet your end.', 'your adventure will begin.', 'you will have a great windfall of cash.', 'the world will come to an end!'];
+
+console.log(`${msgtime[Math.floor(Math.random() * msgtime.length)]} ${msgplace[Math.floor(Math.random() * msgplace.length)]} ${msgaction[Math.floor(Math.random() * msgaction.length)]}`);
+
+
 
