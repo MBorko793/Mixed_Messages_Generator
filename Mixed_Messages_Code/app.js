47bdcc8fc61e6d01c105be6b681b5d21f220d033
MBorko793 <79283620+MBorko793@users.noreply.github.com>
2021-08-19 15:48:35 -0400

    Bug fixes

--- Mixed_Messages_Code/app.js
+++ Mixed_Messages_Code/app.js
@@ -4,7 +4,7 @@ let msgtime = ['In the winter', 'In the summer', 'When the moon reaches its apex
 let msgplace = ['at the mall', 'in a car', 'in the pool', 'in Las Vegas', 'under the apple tree', 'back behind the shed'];
 let msgaction = ['you will meet the love of your life.', 'you will fall into a terrible situation.', 'you will meet your end.', 'your adventure will begin.', 'you will have a great windfall of cash.', 'the world will come to an end!'];
 
-console.log(`${msgtime[Math.floor(Math.random() * msgtime.length)]} ${msgplace[Math.floor(Math.random() * msgplace.length)]} ${msgaction[Math.floor(Math.random() * msgaction.length)]}`);
+console.log(`${msgtime[Math.floor(Math.random() * msgtime.length)]} ${msgplace[Math.floor(Math.random() * msgplace.length)]} ${msgaction[Math.floor(Math.random() * msgtime.length)]}`);
 
 
 
